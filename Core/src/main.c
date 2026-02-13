23020b357a0603e4de64e3b536d86bbe57bc6422
rubin-khadka <rubin.robotic@gmail.com>
2026-02-13 10:06:37 +0100

    update main file

--- Core/src/main.c
+++ Core/src/main.c
@@ -1,10 +1,24 @@
-#include "stm32f10x.h"                  // Device header
+/*
+ * main.c
+ *
+ *  Created on: Feb 12, 2026
+ *      Author: Rubin Khadka
+
+ */
+ 
+#include "stm32f10x.h"		// Device header
+#include "gpio.h"
 
 int main(void)
 {
+	// GPIO PC13 initialization
+	LED_init();
+
 	while(1)
 	{
-		
+		LED_ON();
+		Delay_ms(500);
+		LED_OFF();
+		Delay_ms(500);
 	}
 }
-
